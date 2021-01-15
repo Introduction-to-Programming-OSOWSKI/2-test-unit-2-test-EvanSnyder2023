896eeb3c3bbebc5823260c1f0e746807d9752068
EvanSnyder2023 <816547@mystma.org>
2021-01-15 18:31:50 +0000

    Unit 2 Test try 10

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 def waterState(f):
-    if f < 32:
+    if f <= 32:
         return "solid"
-    elif f > 212:
+    elif f >= 212:
         return "gas"
     else:
         return "liquid"
+
+print(waterState(50))
 
 def isDozen(d):
     if d % 12 == 0:
@@ -12,11 +14,13 @@ def isDozen(d):
     else:
         return False
 
-def toGermen(x):
-    if x == "yes":
+def toGermen(t):
+    if t == "yes":
         return "ja"
     else:
         return "nein"
+
+print(toGermen("no"))
 
 def stopLight(c):
     if c == "green":
