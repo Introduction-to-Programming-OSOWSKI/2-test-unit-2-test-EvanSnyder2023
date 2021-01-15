ec29888006b43613212dd4360a82a629b14938f0
EvanSnyder2023 <816547@mystma.org>
2021-01-15 16:29:03 +0000

    Unit 2 Test try 1

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-#WRITE YOUR CODE IN THIS FILE
+def waterstate(f):
+    if f <= 32:
+        return "solid"
+    elif f >= 212:
+        return "gas"
+    else:
+        return "liquid"
+
+def isDozen(d):
+    if d % 12 == 12:
+        return True
+    else:
+        return False
+
+def toGermen(ja,nein):
+    if True:
+        return ja
+    else:
+        return nein
+
+def stopLight(c):
+    if c == "slow":
+        return "green"
+    elif c == "slow":
+        return "yellow"
+    else:
+        return "blue"
